1488a7797b939865246d85d67b69d906ec9a0f5f
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-12-19 07:56:23 +0200

    feat: add color mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { ColorModeButton } from "./ui/color-mode";
 
 export default function NavBar() {
   return (
     <>
       <HStack bgColor="red.200">
         <Image src={logo} boxSize="50px" />
+        <ColorModeButton />
         <Text>NavBar</Text>
       </HStack>
     </>
