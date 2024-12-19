5a26db72207548577ae0d60cd6c8d4f9cf2cdae3
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-12-19 11:54:42 +0200

    refactor: remove unnecessary fragment

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,23 @@ import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
-    <>
-      <Grid
-        templateAreas={{
-          base: `"nav" "main"`,
-          lg: `"nav nav" "aside main"`,
-        }}
-      >
-        <GridItem area="nav" bg="red">
-          <NavBar />
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="red">
+        <NavBar />
+      </GridItem>
+      <Stack hideBelow="md">
+        <GridItem area="aside" bg="yellow">
+          Aside
         </GridItem>
-        <Stack hideBelow="md">
-          <GridItem area="aside" bg="yellow">
-            Aside
-          </GridItem>
-        </Stack>
-        <GridItem area="main" bg="pink.50">
-          Main
-        </GridItem>
-      </Grid>
-    </>
+      </Stack>
+      <GridItem area="main" bg="pink.50">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
