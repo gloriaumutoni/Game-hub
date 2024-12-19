46e768350979e587e9e0fbd1865d351a913db925
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-12-19 11:50:28 +0200

    feat: implement switch component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
+import { Switch } from "@/components/ui/switch";
 import logo from "../assets/logo.webp";
-import { ColorModeButton } from "./ui/color-mode";
+import { useColorMode } from "./ui/color-mode";
 
 export default function NavBar() {
+  const { toggleColorMode, colorMode } = useColorMode();
   return (
     <>
       <HStack bgColor="red.200">
         <Image src={logo} boxSize="50px" />
-        <ColorModeButton />
+        <Switch onChange={toggleColorMode}>{colorMode}</Switch>
         <Text>NavBar</Text>
       </HStack>
     </>
