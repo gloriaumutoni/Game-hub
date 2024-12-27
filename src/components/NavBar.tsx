1e6d951284a5e4987b14c17941f1c7d559763329
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-12-27 11:12:23 +0200

    feat: build color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,16 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
-import { Switch } from '@/components/ui/switch'
-import logo from '../assets/logo.webp'
-import { useColorMode } from './ui/color-mode'
+import { Switch } from "@/components/ui/switch";
+import { HStack, Image } from "@chakra-ui/react";
+
+import logo from "../assets/logo.webp";
+import { useColorMode } from "./ui/color-mode";
 
 export default function NavBar() {
-  const { toggleColorMode, colorMode } = useColorMode()
+  const { toggleColorMode, colorMode } = useColorMode();
+
   return (
-    <>
-      <HStack bgColor="red.200">
-        <Image src={logo} boxSize="50px" />
-        <Switch onChange={toggleColorMode}>{colorMode}</Switch>
-        <Text>NavBar</Text>
-      </HStack>
-    </>
-  )
+    <HStack bgColor="red.200" justifyContent="space-between" paddingX="7">
+      <Image src={logo} boxSize="50px" />
+      <Switch onChange={toggleColorMode}>{colorMode}</Switch>
+    </HStack>
+  );
 }
