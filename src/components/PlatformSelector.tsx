49a266e4a48372bf7e04a4a5173d3029b6f468e4
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-01-13 09:37:58 +0200

    style: place selector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -5,6 +5,7 @@ import {
   MenuRoot,
   MenuTrigger,
 } from "@/components/ui/menu";
+import { Box } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
 import usePlatforms from "../hooks/usePlatforms";
@@ -24,23 +25,25 @@ export default function PlatformSelector({
   if (error) return null;
 
   return (
-    <MenuRoot>
-      <MenuTrigger asChild>
-        <Button variant="outline" size="sm">
-          {selectedPlatform?.name || "Platforms"} <BsChevronDown />
-        </Button>
-      </MenuTrigger>
-      <MenuContent>
-        {data.map((platform) => (
-          <MenuItem
-            onClick={() => onSelectPlatform(platform)}
-            key={platform.id}
-            value={`${platform.name}`}
-          >
-            {platform.name}
-          </MenuItem>
-        ))}
-      </MenuContent>
-    </MenuRoot>
+    <Box position="relative">
+      <MenuRoot>
+        <MenuTrigger asChild>
+          <Button variant="outline" size="sm">
+            {selectedPlatform?.name || "Platforms"} <BsChevronDown />
+          </Button>
+        </MenuTrigger>
+        <MenuContent>
+          {data.map((platform) => (
+            <MenuItem
+              onClick={() => onSelectPlatform(platform)}
+              key={platform.id}
+              value={`${platform.name}`}
+            >
+              {platform.name}
+            </MenuItem>
+          ))}
+        </MenuContent>
+      </MenuRoot>
+    </Box>
   );
 }
